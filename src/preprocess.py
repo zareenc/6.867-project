8489b0a49e597a72f6ca0d23965878163d162628
Ayesha Bajwa <ayesharbajwa@gmail.com>
2017-11-06 21:09:23 -0500

    removing stopwords, commas and periods, updating dictionary

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,7 +6,7 @@ from get_yelp_data import get_review_data
 
 '''
 Preprocess reviews from csv file and return 
-inputs X and labels Y.
+featurized inputs X and labels Y.
 
 Args:
 csv_file (str): path to csv file containing review data
@@ -22,27 +22,48 @@ def preprocess(csv_file):
     X = np.asarray((n,1))
     Y = np.asarray((n, 1))
 
+    rem_punc = ['.', ',']
+    stopwords = set(nltk.corpus.stopwords.words('english'))
+
+
     errors = 0
     good_ids = []
     tokens = {}
     pos = {}
+    dictionary = {}
 
-    # tokenize and tag parts of speech
+    # clean up by tokenizing and tagging parts of speech
     for i in xrange(n):
+
         row = review_data[i]
         review = row['text']
-        id = row['review_id']
+        review_id = row['review_id']
+        
         try:
-            tokens[id] = nltk.word_tokenize(review)
-            pos[id] = nltk.pos_tag(tokens[id])
-            good_ids.append(id)
+            # separates words from punctuation
+            separated = nltk.word_tokenize(review)
+            tokens[review_id] = [token.lower() for token in separated if token not in stopwords if token not in rem_punc]
+
+            # adds unique tokens to dictionary
+            for token in tokens[review_id]:
+                if token not in dictionary:
+                    dictionary[token] = len(dictionary)
+
+            # tag part of speech or punctuation for each separated item
+            pos[review_id] = nltk.pos_tag(tokens[review_id])
+
+            # save list of ids of correctly preprocessed reviews
+            good_ids.append(review_id)
+        
         except:
             print review
             errors += 1
-            print "Couldn't tokenize review", id
+            print "Couldn't tokenize review", review_id
 
     print "total reviews: %d" % n
     print "total errors: %d" % errors
+    print "dictionary size: %d" % len(dictionary)
+    
     return (X, Y)
 
 if __name__ == "__main__":
