c7f8964fe41851c67d1767df6ead7eac8f2ea796
Ayesha Bajwa <ayesharbajwa@gmail.com>
2017-11-06 22:07:14 -0500

    generating feature matrix, multiclass and binary labels

--- src/preprocess.py
+++ src/preprocess.py
@@ -19,12 +19,9 @@ def preprocess(csv_file):
 
     review_data = get_review_data(csv_file)
     n, = review_data.shape
-    X = np.asarray((n,1))
-    Y = np.asarray((n, 1))
 
     rem_punc = ['.', ',']
     stopwords = set(nltk.corpus.stopwords.words('english'))
-
 
     errors = 0
     good_ids = []
@@ -60,12 +57,40 @@ def preprocess(csv_file):
             errors += 1
             print "Couldn't tokenize review", review_id
 
+    d = len(dictionary)
     print "total reviews: %d" % n
     print "total errors: %d" % errors
-    print "dictionary size: %d" % len(dictionary)
+    print "dictionary size: %d" % d
+
+    # X is feature matrix from the bag of words model
+    # Y_multi is multi-class labels matrix
+    X = np.zeros((n, d))
+    Y_multi = np.zeros((n, 1))
+
+    for i in xrange(n):
+
+        row = review_data[i]
+        review_id = row['review_id']
+        rating = row['stars']
+
+        if review_id in good_ids:
+            for token in tokens[review_id]:
+                if token in dictionary:
+                    X[i][dictionary[token]] = 1
+
+        Y_multi[i] = int(rating)
     
-    return (X, Y)
+    # create binary star ratings where 1-2 is -1 and 3-5 is +1
+    Y_binary = np.where((Y_multi > 2), 1, -1)
+
+    return (X, Y_multi, Y_binary)
 
 if __name__ == "__main__":
     csv_file = 'data/review.csv'
-    X, Y = preprocess(csv_file)
+    X, Y_m, Y_b = preprocess(csv_file)
+    print "X (feature matrix) is: ", X
+    print "Y_m (multi-class labels) is: ", Y_m
+    print "Y_b (binary labels) is: ", Y_b
+
+
+
