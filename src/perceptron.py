2167d76ea8cf234add5373a73308e66b208f2732
Ayesha Bajwa <ayesharbajwa@gmail.com>
2017-11-08 23:11:34 -0500

    added validation to perceptron

--- src/perceptron.py
+++ src/perceptron.py
@@ -6,15 +6,20 @@ from preprocess import *
 import pdb
 
 # testing on toy AZ and NV datasets - AZ has 132 reviews, NV has 60
-training_csv = 'data/filtered_az_reviews.csv'
-test_csv = 'data/filtered_nv_reviews.csv'
+train_csv = 'data/filtered_az_reviews_train.csv'
+val_csv = 'data/filtered_az_reviews_val.csv'
+test_csv = 'data/filtered_az_reviews_test.csv'
 
-pre_train = Preprocessor(training_csv)
+pre_train = Preprocessor(train_csv)
+pre_val = Preprocessor(val_csv)
 pre_test = Preprocessor(test_csv)
 
 pre_train.cleanup()
 dict_train = pre_train.get_dictionary()
 X_TRAIN, Y_TRAIN_MULTI, Y_TRAIN_BINARY = pre_train.featurize(dict_train)
+
+pre_val.cleanup()
+X_VAL, Y_VAL_MULTI, Y_VAL_BINARY = pre_val.featurize(dict_train)
 
 pre_test.cleanup()
 X_TEST, Y_TEST_MULTI, Y_TEST_BINARY = pre_test.featurize(dict_train)
@@ -38,4 +43,5 @@ if __name__ == "__main__":
 	print "w is ", w
 	print "w0 is ", w0
 
-	print get_perceptron_score(model, X_TEST, Y_TEST_BINARY)
+	print "validation: ", get_perceptron_score(model, X_VAL, Y_VAL_BINARY)
+	print "test: ", get_perceptron_score(model, X_TEST, Y_TEST_BINARY)
