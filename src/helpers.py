8b6c5f4d75c7a0c556e7f6d4c8cc8668f4122935
Zareen Choudhury <zareenc@mit.edu>
2017-11-09 10:28:31 -0500

    Add helper for writing classification results to file, results from binary logistic regression

--- src/helpers.py
+++ src/helpers.py
@@ -11,3 +11,37 @@ def binary_classif_err(pred, labels):
     inner_prod[inner_prod == -1] = 1 # -1 -> error
     errors = np.sum(inner_prod)
     return float(errors), float(len(pred) - errors) / len(pred)
+
+
+'''
+Append classification results to file.
+
+Arguments:
+   results_file: filepath of file to write to
+   params_list: list of dicts containing parameters used {str: *}
+   results_list: list of dict containing results {str: *}
+
+Example:
+   params_list = [{'gamma':0.1}, {'gamma':0.01}]
+   results_list = [{'train':0.99,'test':0.80}, {'train':0.99,'test':0.82}]
+   write_results('results.txt', params_list, results_list)
+'''
+def write_results(results_file, params_list, results_list):
+
+    assert len(params_list) == len(results_list)
+
+    with open(results_file, 'a') as f:
+        for i in xrange(len(params_list)):
+            results = results_list[i]
+            params = params_list[i]
+
+            params_str = ''
+            for k, v in params.iteritems():
+                params_str += '%s: %s ' % (k, str(v))
+            f.write(params_str + '\n')
+        
+            results_str = ''
+            for k, v in results.iteritems():
+                results_str += '%s: %s ' % (k, str(v))
+            f.write(results_str + '\n')
+            f.write('\n')
