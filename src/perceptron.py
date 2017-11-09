929625cb9f7ce9e7cc8bce6b998f80f39feaa5e0
Ayesha Bajwa <ayesharbajwa@gmail.com>
2017-11-09 14:30:45 -0500

    updated percrptron to use helpers

--- src/perceptron.py
+++ src/perceptron.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import perceptron
 #from sklearn.linear_model import SGDClassifier
 from preprocess import *
+from helpers import binary_classif_err
 import pdb
 
 # testing on toy AZ and NV datasets - AZ has 132 reviews, NV has 60
-train_csv = 'data/filtered_az_reviews_train.csv'
-val_csv = 'data/filtered_az_reviews_val.csv'
-test_csv = 'data/filtered_az_reviews_test.csv'
+train_csv = '../data/filtered_nv_reviews_train.csv'
+val_csv = '../data/filtered_nv_reviews_val.csv'
+test_csv = '../data/filtered_nv_reviews_test.csv'
 
 pre_train = Preprocessor(train_csv)
 pre_val = Preprocessor(val_csv)
@@ -31,17 +32,33 @@ def train_perceptron(X_train, Y_train):
 	p.fit(X_train, Y_train)
 	return p
 
-def get_perceptron_score(linear_model, X_test, Y_test):
-	return linear_model.score(X_test, Y_test)
+
+def get_perceptron_error(linear_model, X, Y):
+	n,d = X.shape
+	return binary_classif_err(linear_model.predict(X).reshape((n, 1)), Y)
+
 
 
 if __name__ == "__main__":
 	
+	# training
 	model = train_perceptron(X_TRAIN, Y_TRAIN_BINARY)
 	w = model.coef_
 	w0 = model.intercept_
 	print "w is ", w
 	print "w0 is ", w0
 
-	print "validation: ", get_perceptron_score(model, X_VAL, Y_VAL_BINARY)
-	print "test: ", get_perceptron_score(model, X_TEST, Y_TEST_BINARY)
+	err_val, acc_val = get_perceptron_error(model, X_TRAIN, Y_TRAIN_BINARY)
+	print "training errors: ", err_val
+	print "training accuracy: ", acc_val
+
+	# validation
+	err_val, acc_val = get_perceptron_error(model, X_VAL, Y_VAL_BINARY)
+	print "validation errors: ", err_val
+	print "validation accuracy: ", acc_val
+
+	# test
+	err_val, acc_val = get_perceptron_error(model, X_TEST, Y_TEST_BINARY)
+	print "test errors: ", err_val
+	print "test accuracy: ", acc_val
+
