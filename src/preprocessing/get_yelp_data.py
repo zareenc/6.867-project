2524700b13b6d8dd181b9476dcf4aedfa2448da5
Zareen Choudhury <zareenc@mit.edu>
2017-12-11 14:52:52 -0500

    Move data constants into separate file, refactor get_yelp_data

--- src/preprocessing/get_yelp_data.py
+++ src/preprocessing/get_yelp_data.py
@@ -3,60 +3,8 @@ import numpy as np
 import argparse
 import pdb
 import csv
+from yelp_data_constants import *
 
-# constants
-REVIEW_BUSID_IDX = 4
-BUSINESS_BUSID_IDX = 15
-
-REVIEW_NAMES = ('funny', 'user_id', 'review_id', 'text', 'business_id', \
-        'stars', 'date', 'useful', 'cool')
-BUSINESS_NAMES = ('attributes.Ambience.divey', 'attributes.RestaurantsDelivery', \
-        'attributes.DogsAllowed', 'postal_code', 'hours.Thursday', \
-        'attributes.HairSpecializesIn.coloring', 'attributes.BestNights.sunday', \
-        'attributes.BYOB', 'attributes.AgesAllowed', 'attributes.Music.video', \
-        'hours.Friday', 'latitude', 'attributes.Alcohol', \
-        'attributes.Ambience.classy', 'attributes.RestaurantsTableService', \
-        'business_id', 'attributes.Ambience.touristy', \
-        'attributes.RestaurantsCounterService', 'attributes.Corkage', \
-        'attributes.RestaurantsGoodForGroups', 'categories', 'name', \
-        'attributes.BusinessAcceptsBitcoin', 'attributes.HappyHour', \
-        'attributes.WheelchairAccessible', 'attributes.Ambience.hipster', \
-        'attributes.BusinessAcceptsCreditCards', 'is_open', \
-        'attributes.DietaryRestrictions.vegetarian', 'attributes.Music.live', \
-        'attributes.Music.background_music', 'neighborhood', \
-        'attributes.BusinessParking.lot', 'attributes.Music.karaoke', \
-        'review_count', 'attributes.GoodForMeal.breakfast', \
-        'attributes.NoiseLevel', 'attributes.HairSpecializesIn.perms', \
-        'state', 'attributes.DriveThru', 'attributes.HasTV', \
-        'attributes.GoodForMeal.dinner', 'attributes.BusinessParking.street', \
-        'address', 'attributes.RestaurantsAttire', 'hours.Sunday', \
-        'attributes.BestNights.tuesday', 'attributes.AcceptsInsurance', \
-        'attributes.BestNights.wednesday', 'hours.Wednesday', \
-        'attributes.HairSpecializesIn.kids', 'attributes.Open24Hours', \
-        'attributes.Ambience.trendy', 'attributes.CoatCheck', 'hours.Monday', \
-        'attributes.HairSpecializesIn.straightperms', 'city', \
-        'attributes.HairSpecializesIn.curly', 'attributes.Music.no_music', \
-        'hours.Tuesday', 'attributes.HairSpecializesIn.africanamerican', \
-        'stars', 'attributes.RestaurantsPriceRange2', \
-        'attributes.Ambience.intimate', 'attributes.GoodForMeal.latenight', \
-        'attributes.GoodForMeal.dessert', 'attributes.BusinessParking.validated', \
-        'attributes.GoodForMeal.lunch', 'attributes.GoodForKids', \
-        'attributes.DietaryRestrictions.soy-free', 'attributes.GoodForMeal.brunch', \
-        'attributes.BusinessParking.valet', 'longitude', \
-        'attributes.DietaryRestrictions.gluten-free', \
-        'attributes.BYOBCorkage', 'attributes.BusinessParking.garage', \
-        'attributes.BestNights.friday', 'hours.Saturday', 'attributes.Music.dj', \
-        'attributes.HairSpecializesIn.extensions', 'attributes.BestNights.saturday', \
-        'attributes.Ambience.casual', 'attributes.BestNights.thursday', \
-        'attributes.BestNights.monday', 'attributes.HairSpecializesIn.asian', \
-        'attributes.DietaryRestrictions.kosher', 'attributes.WiFi', 'attributes.Smoking', \
-        'attributes.DietaryRestrictions.halal', 'attributes.GoodForDancing', \
-        'attributes.ByAppointmentOnly', 'attributes.Caters', \
-        'attributes.RestaurantsReservations', 'attributes.DietaryRestrictions.dairy-free', \
-        'attributes.DietaryRestrictions.vegan', 'attributes.Ambience.romantic', \
-        'attributes.Music.jukebox', 'attributes.Ambience.upscale', \
-        'attributes.RestaurantsTakeOut', 'attributes.BikeParking', \
-        'attributes.OutdoorSeating');
 
 ''' Helper functions '''
 def construct_filtered_set(txt_file_path):
@@ -86,16 +34,19 @@ def get_filtered_data(data, filter_column_index, filtered_value_set):
             filtered_data.append(d)
     return filtered_data
 
+def get_data(csv_file, names):
+    print("getting data")
+    filling = None
+    data = genfromtxt(csv_file, dtype=None, names=names, \
+                             delimiter='\t', skip_header=1, comments=None, \
+                             filling_values=filling)
+    print("done getting data")
+    return data
+
 
 ''' Review data '''
 def get_review_data(csv_file):
-    filling = None
-    print("getting review data")
-    data = genfromtxt(csv_file, dtype=None, names=REVIEW_NAMES, \
-                             delimiter='\t', skip_header=1, comments=None, \
-                             filling_values=filling)
-    print("done getting review data")
-    return data
+    return get_data(csv_file, REVIEW_NAMES)
 
 def get_filtered_review_data(review_data, filtered_value_set):
     return get_filtered_data(review_data, REVIEW_BUSID_IDX, filtered_value_set)
@@ -103,16 +54,10 @@ def get_filtered_review_data(review_data, filtered_value_set):
 
 ''' Business data '''
 def get_business_data(csv_file):
-    filling = None
-    print("getting business data")
-    data = genfromtxt(csv_file, dtype=None, names=BUSINESS_NAMES, \
-                             delimiter='\t', skip_header=1, comments=None, \
-                             filling_values=filling)
-    print("done getting business data")
-    return data
+    return get_data(csv_file, BUSINESS_NAMES)
 
 def get_filtered_business_data(business_data, filtered_value_set):
-    return get_filtered_data(review_data, BUSINESS_BUSID_IDX, filtered_value_set)
+    return get_filtered_data(business_data, BUSINESS_BUSID_IDX, filtered_value_set)
 
 
 if __name__ == "__main__":
